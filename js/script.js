baff6c150a0ab5f625259aeeb9cef8288ea73695
Eman <emanatiya87@gmail.com>
2025-05-28 20:16:31 +0300

    header sticy + animations scrolling+ counter

---
+++ js/script.js
@@ -0,0 +1,18 @@
+AOS.init();
+let counter1 = document.getElementById("counter1");
+let counter2 = document.getElementById("counter2");
+let counter3 = document.getElementById("counter3");
+function counter(counter, number) {
+  let start = 0;
+  let intervalId = setInterval(function () {
+    counter.innerHTML = start;
+    start += 10;
+
+    if (start > number) {
+      clearInterval(intervalId);
+    }
+  }, 20);
+}
+counter(counter1, 400);
+counter(counter2, 600);
+counter(counter3, 100);
